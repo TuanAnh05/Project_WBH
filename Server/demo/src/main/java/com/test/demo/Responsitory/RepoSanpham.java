c3ad5c2c340ea3af80c54301be78e8d797716823
TsukiHentai <111141519+TsukiHentai@users.noreply.github.com>
2022-09-11 16:35:14 +0700

    Update RepoSanpham.java
    Update query

--- Server/demo/src/main/java/com/test/demo/Responsitory/RepoSanpham.java
+++ Server/demo/src/main/java/com/test/demo/Responsitory/RepoSanpham.java
@@ -3,5 +3,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.test.demo.Models.Sanpham;
 public interface RepoSanpham extends JpaRepository<Sanpham,Long>{
-    
+    @Query(value = "select * from sanpham join danhmucsanpham on sanpham.iddanhmuc=danhmucsanpham.id where danhmucsanpham.id=:iniddanhmuc",nativeQuery = true)
+    List getSanPhamByIdDanhMuc(@Param("iniddanhmuc") Integer iniddanhmuc);
 }
