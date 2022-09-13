1c7d756184fdda906ad1e09250411d0d56b6018c
Nguyen Tuan Anh <110550111+TuanAnh05@users.noreply.github.com>
2022-09-13 17:01:08 +0700

    add file Signin css

--- Client/webbh/src/App.js
+++ Client/webbh/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Page/Home';
+import Login from './Page/Login';
+import DetailProduct from './Page/DetailProduct';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Routes>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/detailProduct' element={<DetailProduct/>}/>
+        </Routes>
+      </Router>
+     
       </div>
   );
 }
